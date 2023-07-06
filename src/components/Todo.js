05862cbeccc4abb6045845baaf6f8785facf00dc
Adam Tak <adam.tak2112@gmail.com>
2023-07-06 11:27:43 -0600

    add styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
+import '../styles/style.css';
+
 class Todo extends React.Component {
     constructor(props){
         super(props);
@@ -14,10 +16,13 @@ class Todo extends React.Component {
 
     render(){
         return(
-            <Container>
+            <Container fluid className='todo-container'>
                 <Row>
                     <Col>
                         <h1>hello!</h1>
+                    </Col>
+                    <Col>
+                        <p>This is another section.</p>
                     </Col>
                 </Row>
             </Container>
